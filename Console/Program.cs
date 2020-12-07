fe872cffa9e43583bc20cda802eba6160691412b
Yanka Larissa Pereira <ylpereira1@stefanini.com>
2020-12-07 18:14:03 -0300

    conteudo do curso de introducao .net core, roteamento, formulário, controler

---
+++ Console/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace NetCoreLab
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            bool helloWorld = true;
+
+            if(helloWorld){
+                Console.WriteLine("Hello World Stefanini!");
+            }else{
+                Console.WriteLine("Bye Carpa!");
+            }
+        }
+    }
+}
---
+++ mvc/Controllers/AdminController.cs
@@ -0,0 +1,43 @@
+using System;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Primitives;
+
+namespace mvc.Controllers
+{
+
+    [Route("academy/admin")]
+    public class AdminController : Controller
+    {
+        [HttpGet("main/{token}/{nome}")]
+        public IActionResult Index(string token, string nome){
+            return Content("Admin Controller " + nome + " token : " + token);
+        }
+
+        [HttpGet("stefanini")]
+        public IActionResult Stefanini(){
+            var name = Request.Query["name"];
+            return Content("My first day at Stefanini! "+ name);
+        }
+
+        [HttpGet("view")]
+        public IActionResult visualizar(){
+            ViewData["autenticado"] = false;
+            return View();
+        }
+
+        [HttpGet("form")]
+        public IActionResult form(){
+            return View();
+        }
+
+        [HttpPost("data")]
+        public IActionResult data(){
+            StringValues name;
+            StringValues email;
+
+            Request.Form.TryGetValue("name", out name);
+            Request.Form.TryGetValue("email", out email);
+            return Content("Formulário enviado com sucesso! " + "\n nome "+ name + "\n email "+ email );
+        }
+    }
+}
